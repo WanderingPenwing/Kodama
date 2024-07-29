4dbe68e9ec7ff6b6a2f7dd1e15e3abfd016faec7
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-07-29 13:38:22 +0200

    transparent

--- config.h
+++ config.h
@@ -93,7 +93,7 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-static const int alpha = 0xff;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
