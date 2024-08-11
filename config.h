09cbf6d8ad4fe5cb5f5cd4745610f093ae0a3903
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-08-11 03:17:47 +0200

    keyboard sel

--- config.h
+++ config.h
@@ -200,6 +200,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,			XK_V,		   clippaste,	  {.i =  0} },
 	{ TERMMOD,			XK_Y,		   selpaste,	   {.i =  0} },
 	{ TERMMOD,			XK_Num_Lock,	numlock,		{.i =  0} },
+	{ TERMMOD,			XK_Escape,      keyboard_select,{ 0 } },
+	
 };
 
 /*
